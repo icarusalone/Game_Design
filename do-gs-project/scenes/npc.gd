cbcc0169de25477cb84e23dab73a8c623d14d19a
moriw77 <moriw77@zedat.fu-berlin.de>
2025-11-17 12:15:38 +0900

    tried bugfixing, no success

--- do-gs-project/scenes/npc.gd
+++ do-gs-project/scenes/npc.gd
@@ -2,12 +2,19 @@ extends CharacterBody3D
 
 var up = true
 
+var target_position: Vector3
+var move_time := 3.0
+var elapsed_time := 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var order_pos = Vector3(1, 1, -1)
 	var tween = create_tween()
 	tween.tween_property(self, "position", order_pos, 3)
 	tween.play()
+	
+	target_position = Vector3(1, 1, -1)
+	elapsed_time = 0.0
 	
 	DialogueManager.show_dialogue_balloon(load("res://dialogues/testdialogue.dialogue")) #plays the testdialogue
 
@@ -22,4 +29,5 @@ func _process(delta: float) -> void:
 		up = false
 	if (position.y <= 1):
 		up = true
-	print(position.x, position.y, position.z)
+	
+	#print(position.x, position.y, position.z)
