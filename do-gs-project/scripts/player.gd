4dabe0e54ff0aa2d94f0fd7d335571d16d292a0e
moriw77 <moriw77@zedat.fu-berlin.de>
2025-11-26 11:33:01 +0900

    tried fixing the camera movement bug, failed, bug still there

--- do-gs-project/scripts/player.gd
+++ do-gs-project/scripts/player.gd
@@ -15,7 +15,7 @@ const JUMP_VELOCITY = 4.5
 func _ready():
 	pass
 
-func  _unhandled_input(event: InputEvent) -> void:
+func  _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #capture mouse inputs
 	elif event.is_action_pressed("ui_cancel"): 
@@ -32,13 +32,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
  
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() #determine directions relative to neck
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
