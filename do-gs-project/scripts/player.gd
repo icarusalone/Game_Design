41d756c7bd0a5af53cc732daa9211529577133d0
moriw77 <moriw77@zedat.fu-berlin.de>
2025-11-29 14:04:17 +0900

    added walls to limit player movement

--- do-gs-project/scripts/player.gd
+++ do-gs-project/scripts/player.gd
@@ -28,8 +28,8 @@ func  _input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+	#	velocity += get_gravity() * delta
  
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
